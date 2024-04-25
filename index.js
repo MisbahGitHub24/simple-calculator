5c8daf0930ceb3f474cd7ca8c02fbcb19370d81e
MisbahGitHub24 <160399812+MisbahGitHub24@users.noreply.github.com>
2024-04-25 18:10:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators",
+        type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        name: "operators",
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(`Your answer is : ${answer.firstNumber + answer.secondNumber}`);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(`Your answer is : ${answer.firstNumber - answer.secondNumber}`);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(`Your answer is : ${answer.firstNumber * answer.secondNumber}`);
+}
+else if (answer.operators === "Division") {
+    console.log(`Your answer is : ${answer.firstNumber / answer.secondNumber}`);
+}
+else {
+    console.log("Please select valid operator");
+}
